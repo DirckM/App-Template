7b70c97fb8f913cec4db3a3c347a78ddb953a5a9
Dirck Mulder <dirckmulder20@gmail.com>
2025-07-28 20:31:13 +0200

    prettier on SQL

--- expo-app-template/db/migrations/0000_married_romulus.sql
+++ expo-app-template/db/migrations/0000_married_romulus.sql
@@ -1,13 +1,13 @@
 CREATE TABLE "profiles" (
-	"id" uuid PRIMARY KEY NOT NULL,
-	"username" text,
-	"firstname" text,
-	"lastname" text,
-	"bio" text,
-	"profile_image_url" text,
-	"country" text,
-	"date_of_birth" date,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "profiles_username_unique" UNIQUE("username")
+  "id" uuid PRIMARY KEY NOT NULL,
+  "username" text,
+  "firstname" text,
+  "lastname" text,
+  "bio" text,
+  "profile_image_url" text,
+  "country" text,
+  "date_of_birth" date,
+  "created_at" timestamp DEFAULT now() NOT NULL,
+  "updated_at" timestamp DEFAULT now() NOT NULL,
+  CONSTRAINT "profiles_username_unique" UNIQUE ("username")
 );
